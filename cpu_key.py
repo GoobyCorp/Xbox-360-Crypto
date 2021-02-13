55f5fea680c7f003d3345c20252594b9ab258382
John <pls@gooby.co>
2021-02-13 15:55:41 -0500

    Cleaned up cpu_key.py

--- cpu_key.py
+++ cpu_key.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
 
-from XeCrypt import XeCryptCpuKeyGen  # , XeCryptHammingWeight, XeCryptUidEccEncode, XeCryptCpuKeyValid
+from XeCrypt import XeCryptCpuKeyGen
 
 def main() -> None:
 	key = XeCryptCpuKeyGen()
 	print(key.hex().upper())
-	#print("\\x" + "\\x".join([f"{x:02X}" for x in key]))
-
-	"""
-	test = bytes.fromhex("BADA5535BADA5535BADA550007000000")
-	print(hex(XeCryptHammingWeight(test)))
-	test = XeCryptUidEccEncode(test)
-	print(test.hex().upper())
-	print(XeCryptCpuKeyValid(test))
-	"""
 
 if __name__ == "__main__":
 	main()
