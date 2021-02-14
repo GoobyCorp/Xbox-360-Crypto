9603dac2234d6175cd043e3aef428157a3afffdf
John <pls@gooby.co>
2021-02-13 20:43:28 -0500

    Simplified kv_tool.py

--- kv_tool.py
+++ kv_tool.py
@@ -20,28 +20,18 @@ def main() -> None:
 	print("CPU key: " + cpu_key.hex().upper())
 	print("DVD key: " + dvd_key.hex().upper())
 
-	fuse_lines = [
-		"C0FFFFFFFFFFFFFF",
-		"0000000000000000",  # line #2 - console type
-		"0000000000000000",
-		"0000000000000000",  # line #4 - CPU #1
-		"0000000000000000",  # line #5 - CPU #2
-		"0000000000000000",  # line #6 - CPU #3
-		"0000000000000000",  # line #7 - CPU #4
-		"F000000000000000",
-		"0000000000000000",
-		"0000000000000000",
-		"0000000000000000",
-		"0000000000000000"
-	]
-	fuse_data = bytearray(b"".join([bytes.fromhex(x) for x in fuse_lines]))
+	# create fuse buffer 12 lines by 8 bytes
+	fuse_data = bytearray(12 * 8)
+	pack_into("8s", fuse_data, 0, bytes.fromhex("C0FFFFFFFFFFFFFF"))  # line #1
+	pack_into("1s", fuse_data, 0x38, b"\xF0")  # line #8
 
+	# read the KV
 	kv_path = Path("KV/banned.bin")
 	kv_data = bytearray(kv_path.read_bytes())
 
-	fuse_path = Path("Output/Zero/fuses.bin")
-
+	# update the DVD key
 	pack_into("16s", kv_data, 0x100, dvd_key)
+	# encrypt the KV with the specified CPU key
 	kv_data = XeCryptKeyVaultEncrypt(cpu_key, kv_data)
 
 	# update console type
@@ -58,15 +48,22 @@ def main() -> None:
 	# update CPU key in fuses
 	pack_into("8s8s8s8s", fuse_data, 0x18, cpu_key[:8], cpu_key[:8], cpu_key[8:16], cpu_key[8:16])
 
+	# setup fuse path
+	fuse_path = Path("Output/Zero/fuses.bin")
+	# write fuses
 	fuse_path.write_bytes(fuse_data)
+	# setup KV path
 	kv_path = Path("Output/Zero/kv_enc.bin")
+	# write the KV
 	kv_path.write_bytes(kv_data)
 
+	# print fuse lines
 	print()
 	print("Fuses:")
 	for i in range(12):
 		print(fuse_data[i * 8:(i * 8) + 8].hex().upper())
 
+	# print output paths
 	print()
 	print(f"KV written to \"{str(kv_path.absolute())}\"!")
 	print(f"Fuses written to \"{str(fuse_path.absolute())}\"!")
